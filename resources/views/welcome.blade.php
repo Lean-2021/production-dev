443092ff3f8dc7391c59862e1c62820b77dce574
Lean-2021 <leanw1711@gmail.com>
2025-02-18 12:20:08 -0300

    pagina

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Prueba</title>
 
     <style>
@@ -21,6 +23,42 @@
 <body>
 <h1>Estas en la vista Home de la ruta home</h1>
 <h3>Este framework es: {{ $name }}</h3>
+
+
+<button type="button" class="btn btn-lg btn-danger" data-bs-toggle="popover" data-bs-title="Popover title"
+        data-bs-content={!!<iframe class="img-thumbnail" width="560" height="315"
+        src="https://www.youtube.com/embed/aAxlsmHtro4?si=jpJ12cLSIZVWIpJm"
+        title="YouTube video player" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>!!}>Click to toggle popover
+</button>
+
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+    Launch static backdrop modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+     aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <iframe class="img-thumbnail" width="560" height="315"
+                        src="https://www.youtube.com/embed/aAxlsmHtro4?si=jpJ12cLSIZVWIpJm"
+                        title="YouTube video player" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+
+        </div>
+    </div>
+</div>
 {{--<ul>--}}
 {{--    @foreach( $posts as $index => $post)--}}
 {{--        --}}{{--    <span>{{ $loop->last ? $post['title'] : $post['title'].' | ' }} </span>--}}
@@ -57,6 +95,16 @@
 
     <input type="text" @readonly(false)>
 </form>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+
+<script>
+    const popoverTriggerList = document.querySelectorAll(' [data-bs-toggle="popover" ]');
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+</script>
 </body>
 {{--@includeIf('prueba',['color' => 'red'])--}}
 @includeWhen(true, 'prueba', ['color'=> 'blue']) {{-- Se incluye cuando se cumple la condición }} --}}
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,22 @@
 <?php
 
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\HomeController;
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\ContactController;
+    use App\Http\Controllers\HomeController;
+    use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::get('/', HomeController::class)
-    ->name('home');
+//    Route::get('/', HomeController::class)
+//        ->name('home');
+
+//    Route::redirect('/', function () {
+//        redirect()->route('home');
+//    });
+    Route::view('*', 'errors.404');
+    //    Route::redirect('/', '/error');
 
 
-Route::resource('contacto', ContactController::class)
-    ->except('show');
+    //    Route::resource('contacto', ContactController::class)
+    //        ->except('show');
